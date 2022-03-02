6efb3dbcc287653bd185ff399b5def76cd4a06f2
teixeiraf <fernando.teixeira@verivox.com>
2022-03-02 14:16:26 +0100

    fix in default test config

--- testConfig/default_test_config.py
+++ testConfig/default_test_config.py
@@ -1,11 +1,11 @@
-DRAGON_INFO_RAMP = {
+DEFAULT_RAMP = {
     "time_limit_in_seconds": 600,
     "spawn_rate": 1,
     "ramp_every_X_seconds": 60,
     "initial_users": 1,
     "max_users": 10
 }
-DRAGON_INFO_KPIS = {
+DEFAULT_KPIS = {
     "requests_per_second": 1,
     "fail_ratio_allowed": 0.01,
     "response_time_percentile_90_milliseconds": 1000,
