aa0c5ab358dface96327aa3b9908b8e41b64f311
teixeiraf <fernando.teixeira@verivox.com>
2022-03-02 14:36:55 +0100

    adjusted test configs

--- testConfig/dragon_info_test_config.py
+++ testConfig/dragon_info_test_config.py
@@ -1,12 +1,12 @@
 DRAGON_INFO_RAMP = {
-    "time_limit_in_seconds": 600,
+    "time_limit_in_seconds": 60,
     "spawn_rate": 1,
     "ramp_every_X_seconds": 60,
     "initial_users": 1,
     "max_users": 10
 }
 DRAGON_INFO_KPIS = {
-    "requests_per_second": 1,
+    "requests_per_second": 0.5,
     "fail_ratio_allowed": 0.01,
     "response_time_percentile_90_milliseconds": 1000,
     "response_time_percentile_95_milliseconds": 1500,
