6e54dc1db4222ecfb91d73758229d7c0d5b4eb3c
fernando <fernandoadt1@gmail.com>
2022-04-17 09:38:26 +0200

    added previous launches info test

---
+++ testConfig/previous_launches_test_config.py
@@ -0,0 +1,14 @@
+PREVIOUS_LAUNCHES_INFO_RAMP = {
+    "time_limit_in_seconds": 60,
+    "spawn_rate": 1,
+    "ramp_every_X_seconds": 60,
+    "initial_users": 1,
+    "max_users": 10
+}
+PREVIOUS_LAUNCHES_INFO_KPIS = {
+    "requests_per_second": 0.1,
+    "fail_ratio_allowed": 0.01,
+    "response_time_percentile_90_milliseconds": 3000,
+    "response_time_percentile_95_milliseconds": 4000,
+    "response_time_percentile_99_milliseconds": 5000
+}
